1ccda419c0326d08dab67221b3750fa15a2a7ddc
IlushinV <122991544+IlushinV@users.noreply.github.com>
2023-01-24 16:41:32 +0300

    24.01.2023

--- restasite/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from restasite.models import MenuItem
-
-admin.site.register(MenuItem)
--- restasite/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RestasiteConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restasite'
