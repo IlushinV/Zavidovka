d5b6180a671a1228115dbfb0b54851556676ea67
IlushinV <122991544+IlushinV@users.noreply.github.com>
2023-01-24 16:55:25 +0300

    24.01.2023

---
+++ Zavidovka_site/admin.py
@@ -0,0 +1,5 @@
+# from django.contrib import admin
+#
+# from Zavidovka_site.models import MenuItem
+#
+# admin.site.register(MenuItem)
---
+++ Zavidovka_site/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Zavidovka_siteConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Zavidovka_site'
